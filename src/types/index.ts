173e9a53649d62f66c2f0d12bfd3fc660bda002d
Suprith Reddy <suprith@tryfinancially.com>
2025-05-31 14:30:20 +0200

    updated region code

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,7 +7,7 @@ export interface Country {
   isoAlpha2SubDivisionCode: string;
   region: string;
   subRegion: string;
-  region_code: string;
+  regionCode: string;
   subRegionCode: string;
   flagUnicode: string;
   flagUrl: string;
